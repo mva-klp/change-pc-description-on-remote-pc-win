58bcff3069a60f26973a118e0360438f36c0e1e7
Vladimir Markov <84769092+mva-klp@users.noreply.github.com>
2022-10-15 15:17:38 +0300

    Update Description_PC.bat
    Inserted comments

--- Description_PC.bat
+++ Description_PC.bat
@@ -3,11 +3,16 @@ Set /P $InputPC="Input PC name: "
 echo %$InputPC%
 Set /P $InputDescr="Input Description: "
 echo %$InputDescr%
+rem Создаем файл реестра, содержащий Description, на диске С машины с которой будем менять описание удаленной машины
 echo reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\lanmanserver\parameters /v srvcomment /t REG_SZ /d "%$InputDescr%" /f > C:\reg.bat
-xcopy "C:\reg.bat" "\\%$InputPC%\d$"
-wmic /node:"%$InputPC%" process call create "D:\reg.bat"
+rem Копируем готовый файл на удаленную машину
+xcopy "C:\reg.bat" "\\%$InputPC%\c$"
+rem Запускаем процесс записи исправления в реестре на удаленной машине
+wmic /node:"%$InputPC%" process call create "C:\reg.bat"
+rem Пауза для ожидания отработки процесса
 pause
+rem Подчищаем за собой
 del /f /q "C:\reg.bat"
-del /f /q "\\%$InputPC%\d$\reg.bat"
+del /f /q "\\%$InputPC%\c$\reg.bat"
 echo reg files was deleted
 pause
